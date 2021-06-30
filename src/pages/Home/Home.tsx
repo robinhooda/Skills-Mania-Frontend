c939290bd75f464654d41d324538e55b32fcd2e8
robinhooda <robinhooda66@gmail.com>
2021-06-30 18:08:43 +0530

    Add. navbar and lamding page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-
+import TestSVG from "../../assets/images/test.svg"
+import "./Home.css"
 function Home() {
     return (
-        <div>
-            Home Page
-        </div>
+        <header className="header">
+            <div>
+                <img src={TestSVG} alt="test-pic" className="hero-image" />
+            </div>
+            <div className="header-content">
+                <h2>Test your programming skills</h2>
+                <button className="primary-button large-button">
+                    Explore categories 
+                    <i className="fas fa-arrow-right pad-l-sm"></i>
+                </button>
+            </div>
+        </header>
     )
 }
 
