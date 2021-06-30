0668ff9fd96153e2ab3e50d710d8ed434bc971c7
robinhooda <robinhooda66@gmail.com>
2021-06-30 18:07:28 +0530

    Add. Home Landing page

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import './Navbar.css'
+import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+const Navbar = () => {
+  const { pathname } = useLocation()
 
-function Navbar() {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
+//   const { auth, setAuth } = useAuth()
+
+//   const logout = () => {
+//     setAuth(null)
+//     localStorage.removeItem("auth-token");
+//   }
+const auth = false;
+
+  return (
+    <nav className='navbar'>
+      <div className='navbar-brand'>
+        <div className='navbar-brand--name  bold'>Skills-Mania</div>
+      </div>
+      <>
+        {true ? (
+          <button className='flat-button' onClick={()=>{}}>
+            Login
+          </button>
+        ) : (
+          <>
+            <div>
+              <Link
+                className={pathname === '/login' ? 'links bold' : 'links'}
+                to='/login'
+              >
+                Login
+              </Link>
+            </div>
+            <Link
+              className={pathname === '/signUp' ? 'links bold' : 'links'}
+              to='/signUp'
+            >
+              <button className='transparent-button'>Register</button>
+            </Link>
+          </>
+        )}
+     </>
+    </nav>
+  )
 }
 
 export default Navbar
