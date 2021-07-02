e4b0a5638481569f7fd98297681f7441da6bdf43
robinhooda <robinhooda66@gmail.com>
2021-07-03 02:51:30 +0530

    Add. Page404

--- src/pages/Page404/Page404.tsx
+++ src/pages/Page404/Page404.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
-
-function Page404() {
-    return (
-        <div>
-            not found
-        </div>
-    )
+import NotFound404 from '../../assets/images/404-not-found.svg'
+import { Link } from 'react-router-dom'
+import "./Page404.css"
+const Page404 = () => {
+  return (
+    <div className='center'>
+      <img src={NotFound404} alt='404 Not Found' className='noItemsFoundImg' />
+      <h2 className='mar-sm'>Page Not Found</h2>
+      <div className='text-center line-height-190'>
+        <div>We're sorry the page you requested could not be found</div>
+        <div>Please go back to the homepage</div>
+        <Link className='links' to="/">
+          <button className='large-button primary-button mar-t-sm'>
+            Go to Home
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Page404
