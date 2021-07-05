d0fdaa0cc8bfe3f68ade5b9dee8f9008d80f7a45
robinhooda <robinhooda66@gmail.com>
2021-07-06 00:36:28 +0530

    Add. Highscore page

--- src/pages/Category/Category.tsx
+++ src/pages/Category/Category.tsx
@@ -3,12 +3,22 @@ import CategoryHeroImage from "../../assets/images/Category.svg"
 import { useData } from "../../contexts/DataContext";
 import { Quiz } from "../../data/quiz.types";
 import { Link } from "react-router-dom"
+import { url } from "inspector";
 
 type QUIZ_CARD = { quizItem: Quiz,index:Number };
 function CategoryCard({ quizItem,index }: QUIZ_CARD) {
+//     const categoryBackgroundMask={
+//         backgroundImage: "url(" + quizItem.coverImageUrl + ")",
+//         backgroundRepeat: "no-repeat",
+//         backgroundSize: "100px 150px",
+//    }
     return  (
         <Link to={`/quiz/${quizItem.id}`} className="links">
-            <div className="category-card">
+            <div className="category-card" 
+            // style={categoryBackgroundMask}
+            >
+                {/* TO DO- ADD COVERIMAGE IN BACKGROUND */}
+                {/* <img src={quizItem.coverImageUrl} alt="category-cover-image" className="category-cover-image"/> */}
                 <h3>Category {index}</h3>
                 <h2>{quizItem.name}</h2>
             </div>
---
+++ src/pages/HighScore/HighScore.tsx
@@ -0,0 +1,9 @@
+function HighScore() {
+    return (
+        <div>
+            Coming soon...
+        </div>
+    )
+}
+
+export default HighScore
