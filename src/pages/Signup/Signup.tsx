3697a3ea26cd3bb08a8718803f05ab6261a40237
robinhooda <robinhooda66@gmail.com>
2021-07-05 13:34:20 +0530

    Add. SignUp Page

--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Signup.css'
+import { Link, useNavigate } from 'react-router-dom'
+import Loader from '../../components/Loader/Loader'
+import axios from 'axios'
+const Signup = () => {
+  const [userData, setUserData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  })
+  const [isloading, setIsloading] = useState(false)
+  const navigate = useNavigate()
 
-function Signup() {
-    return (
-        <div>
-            signup
-        </div>
-    )
+  const inputEvent = (event:React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    const name = event.target.name
+
+    setUserData((prev) => {
+      switch (name) {
+        case 'firstName':
+          return {
+            ...userData,
+            firstName: value,
+          }
+        case 'lastName':
+          return {
+            ...userData,
+            lastName: value,
+          }
+        case 'email':
+          return {
+            ...userData,
+            email: value,
+          }
+        case 'password':
+          return {
+            ...userData,
+            password: value,
+          }
+        default:
+          return userData
+      }
+    })
+  }
+
+  const createAccount = async (event:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setIsloading(true)
+    try {
+      const response = await axios.post('http://localhost:3020/signup', {
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        password: userData.password,
+      })
+      console.log({ response })
+      setUserData({ firstName: '', lastName: '', email: '', password: '' })
+      navigate('/login')
+    } catch (err) {
+      console.log(err)
+    }
+    setIsloading(false)
+  }
+
+  return isloading ? (
+    <Loader />
+  ) : (
+    <div className='signup'>
+      <div className='signup-wrapper'>
+        <h1 className='signup-heading dark-blue'>Create an account</h1>
+        <input
+          type='text'
+          name='firstName'
+          onChange={inputEvent}
+          value={userData.firstName}
+          placeholder='First Name'
+          className='form-input-text'
+        />
+
+        <input
+          type='text'
+          name='lastName'
+          onChange={inputEvent}
+          value={userData.lastName}
+          placeholder='Last Name'
+          className='form-input-text'
+        />
+
+        <input
+          type='email'
+          name='email'
+          onChange={inputEvent}
+          value={userData.email}
+          placeholder='email : abcd@gmail.com'
+          className='form-input-text'
+        />
+        <input
+          type='password'
+          name='password'
+          onChange={inputEvent}
+          value={userData.password}
+          placeholder='password'
+          className='form-input-text'
+        />
+        <button className='full-width-primary-button' onClick={createAccount}>
+          CREATE AN ACCOUNT
+        </button>
+        <small>
+          Have an account?
+          <Link className='links' to='/login'>
+            <span className='bold underlined dark-blue pad-l-xs'>Login</span>
+          </Link>
+        </small>
+      </div>
+    </div>
+  )
 }
 
 export default Signup
